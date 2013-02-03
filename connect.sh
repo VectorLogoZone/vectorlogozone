67c66ea175b9e88d3b0ed9669bee393b8e109712
Andrew M <fileformat@gmail.com>
2013-02-03 10:43:53 -0500

    switch to NearlyFreeSpeech for hosting

---
+++ connect.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#
+#
+ssh -i /etc/fileformatnet/nfsnet.pem fileformat_hackerlogos@ssh.phx.nearlyfreespeech.net
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 #
+# deploy to NearlyFreeSpeech.net
+#
+jekyll
+rsync \
+	--archive \
+	--compress \
+	--delete \
+	--ignore-times \
+	--itemize-changes \
+	--rsh "ssh -i /etc/fileformatnet/nfsnet.pem" \
+	--verbose \
+	_site/ \
+	fileformat_hackerlogos@ssh.phx.nearlyfreespeech.net:/home/public/
+
+#
+# old version that deploys to github-pages
+#
 # this depends on the local branch being gh-pages (i.e. git checkout gh-pages)
 #
-git push origin +:
+#git push origin +:
