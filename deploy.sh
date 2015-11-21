3af6f1e2fc9757bb5898a87fb771dd75e3fda850
Andrew M <fileformat@gmail.com>
2015-11-20 19:30:51 -0500

    switched to deploy to s3 for VLZ

--- deploy.sh
+++ deploy.sh
@@ -5,22 +5,17 @@
 
 jekyll build --source www 
 
-rsync \
-	--archive \
-	--compress \
-	--delete \
+# to test, add:
+#	--dry-run \
+
+s3cmd \
+	sync \
+	--delete-removed \
+	--exclude '*~' \
 	--exclude '*_src*' \
 	--exclude '*.sh' \
-	--ignore-times \
-	--itemize-changes \
-	--rsh "ssh -i /etc/fileformatnet/nfsnet.pem" \
+	--no-preserve \
+	--recursive \
 	--verbose \
 	_site/ \
-	fileformat_hackerlogos@ssh.phx.nearlyfreespeech.net:/home/public/
-
-#
-# old version that deploys to github-pages
-#
-# this depends on the local branch being gh-pages (i.e. git checkout gh-pages)
-#
-#git push origin +:
+	s3://www.vectorlogo.zone
