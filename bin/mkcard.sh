f37a5c9638b8eecdd1c52c635b45d7a7c6810513
Andrew Marcuse <fileformat@gmail.com>
2017-02-25 13:30:39 -0500

    script to create card images

---
+++ bin/mkcard.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# utility to make images for OpenGraph & Twitter cards
+#
+# http://superuser.com/questions/516095/bake-an-svg-image-into-a-png-at-a-given-resolution
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+ROOT=../www/logos
+
+find ${ROOT} -name '*-ar21.svg' -print0 |
+    while IFS= read -r -d $'\0' SVG; do
+
+        CARD=${SVG%ar21.svg}card.png
+        #echo "DEBUG: processing ${SVG} to ${CARD}"
+
+        if [ -e "${CARD}" ] ; then
+            echo "DEBUG: existing card found (${CARD})"
+        else
+            echo "INFO: creating new card ${CARD}"
+            #NO: (crops images): convert -density 1200 -resize 480x240 ${SVG} ${CARD}
+            rsvg --dpi-x=1200 --dpi-y=1200 --width=480 --height=240 --format=png ${SVG} ${CARD}
+            mogrify -comment "This image is from [VectorLogoZone](http://www.vectorlogo.zone/).  Enjoy!" ${CARD}
+        fi
+    done
