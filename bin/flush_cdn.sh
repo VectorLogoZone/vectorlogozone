253eedfbf9f2960aafe50cb3955269630149820f
Andrew Marcuse <fileformat@gmail.com>
2021-07-13 07:16:37 -0400

    flush cdn after deploy

--- bin/flush_cdn.sh
+++ bin/flush_cdn.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 #
-# flush the CloudFlare CDN with cURL.  Needs CLOUDFLARE_EMAIL, CLOUDFLARE_API_KEY and CLOUDFLARE_ZONE_ID to be set
+# flush the CloudFlare CDN with cURL.
 #
-#LATER: check that env vars exist, etc
 
-curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_ID}/purge_cache" \
-    -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
-    -H "X-Auth-Key: ${CLOUDFLARE_API_KEY}" \
-    -H "Content-Type: application/json" \
-    --data '{"purge_everything":true}'
+set -o errexit
+set -o pipefail
+set -o nounset
+
+SCRIPT_HOME="$( cd "$( dirname "$0" )" && pwd )"
+ENV_FILE="${SCRIPT_HOME}/../.env"
+
+if [ -f "${ENV_FILE}" ];
+then
+    echo "INFO: loading .env"
+    export $(cat "${ENV_FILE}")
+fi
+
+echo "INFO: flushing cache"
+RESULT=$(
+    curl \
+        --data '{"purge_everything":true}' \
+        --header "Authorization: Bearer ${CLOUDFLARE_API_TOKEN}" \
+        --header "Content-Type: application/json" \
+        --request POST  \
+        --show-error \
+        --silent \
+        "https://api.cloudflare.com/client/v4/zones/${CLOUDFLARE_ZONE_ID}/purge_cache"
+    )
+echo "INFO: result is $(echo "${RESULT}" | jq --compact-output .)"
+
+SUCCESS=$(echo "${RESULT}" | jq --raw-output .success)
+if [ "${SUCCESS}" != "true" ];
+then
+    echo "ERROR: flush failed! (${SUCCESS})"
+    exit 1
+fi
+
+echo "INFO: cache flush complete"
