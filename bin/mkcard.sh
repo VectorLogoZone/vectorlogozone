08a0efc3b48f58a80e9b3ffb69369fab776f91da
Andrew Marcuse <fileformat@gmail.com>
2021-12-03 17:58:42 -0500

    FEATURE: raster images for social media metadata

--- bin/mkcard.sh
+++ bin/mkcard.sh
@@ -8,20 +8,41 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-ROOT=../www/logos
+if ! [ -x "$(command -v rsvg-convert)" ]; then
+  echo "ERROR: rsvg-convert is not installed.  Try 'brew install librsvg' or 'apt-get install librsvg2-bin'"
+  exit 1
+fi
 
-find ${ROOT} -name '*-ar21.svg' -print0 |
+if ! [ -x "$(command -v mogrify)" ]; then
+  echo "ERROR: mogrify is not installed.  Try 'brew install imagemagick' or 'apt-get install imagemagick'"
+  exit 1
+fi
+
+SCRIPT_HOME="$( cd "$( dirname "$0" )" && pwd )"
+WWW_HOME=$(realpath "${SCRIPT_HOME}/../www")
+
+find ${WWW_HOME} -name '*-ar21.svg' -print0 |
+    sort -z |
     while IFS= read -r -d $'\0' SVG; do
 
-        CARD=${SVG%ar21.svg}card.png
+        CARD=${SVG%.svg}.png
         #echo "DEBUG: processing ${SVG} to ${CARD}"
+        URLPATH=${SVG#"${WWW_HOME}"}
 
         if [ -e "${CARD}" ] ; then
             echo "DEBUG: existing card found (${CARD})"
         else
             echo "INFO: creating new card ${CARD}"
             #NO: (crops images): convert -density 1200 -resize 480x240 ${SVG} ${CARD}
-            rsvg --dpi-x=1200 --dpi-y=1200 --width=480 --height=240 --format=png "${SVG}" "${CARD}"
-            mogrify -comment "This image is from [VectorLogoZone](https://www.vectorlogo.zone/).  Enjoy!" "${CARD}"
+            rsvg-convert \
+                --background-color=white \
+                --dpi-x=1200 \
+                --dpi-y=1200 \
+                --width=1200 \
+                --height=600 \
+                --format=png \
+                "${SVG}" \
+                >"${CARD}"
+            mogrify -comment "This image is from [VectorLogoZone](https://www.vectorlogo.zone${URLPATH}).  Enjoy!" "${CARD}"
         fi
     done
